2fec8293792bcc591253ebb94580245258750483
Caio Dottori <caio.dottori@starkbank.com>
2020-04-06 15:38:33 -0300

    added utils/url

---
+++ lib/utils/url.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module StarkBank
+  module Utils
+    module URL
+      # generates query string from hash
+      def self.urlencode(params)
+        clean_params = {}
+        params.each do |key, value|
+          next if value.nil?
+
+          clean_params[StarkBank::Utils::Case.snake_to_camel(key)] = value.is_a?(Array) ? value.join(',') : value
+        end
+
+        return '' if clean_params.empty?
+
+        query_list = []
+        clean_params.each do |key, value|
+          query_list << "#{key}=#{value}"
+        end
+        '?' + query_list.join('&')
+      end
+    end
+  end
+end
