d0c965231215e7226db010d1d6fc3637d37b0f4c
Caio Dottori <caio.dottori@starkbank.com>
2020-06-05 11:27:56 -0300

    version 0.4.0

--- lib/utils/request.rb
+++ lib/utils/request.rb
@@ -61,7 +61,7 @@ module StarkBank
         req['Access-Time'] = access_time
         req['Access-Signature'] = signature
         req['Content-Type'] = 'application/json'
-        req['User-Agent'] = "Ruby-#{RUBY_VERSION}-SDK-0.3.0"
+        req['User-Agent'] = "Ruby-#{RUBY_VERSION}-SDK-0.4.0"
         req['Accept-Language'] = language
 
         request = Net::HTTP.start(uri.hostname, use_ssl: true) { |http| http.request(req) }
