4f5736f40b26784e33fd21d2bdda18d7d61f810a
gaia-stark <rafael.gaia@starkbank.com>
2023-05-09 16:30:37 -0300

    adds purpose and schedule attributes to accessor attr_reader in corporaterule resource.

--- lib/corporate_rule/corporaterule.rb
+++ lib/corporate_rule/corporaterule.rb
@@ -14,7 +14,7 @@ module StarkBank
   # ## Parameters (optional):
   # - interval [string, default 'lifetime']: interval after which the rule amount counter will be reset to 0. ex: 'instant', 'day', 'week', 'month', 'year' or 'lifetime'
   # - schedule [string, default nil]: schedule time for user to spend. ex: "every monday, wednesday from 00:00 to 23:59 in America/Sao_Paulo"
-  # - @TODO: default [] or nil??? purposes [list of string, default nil]: list of strings representing the allowed purposes for card purchases, you can use this to restrict ATM withdrawals. ex: ["purchase", "withdrawal"]
+  # - purposes [list of string, default []]: list of strings representing the allowed purposes for card purchases, you can use this to restrict ATM withdrawals. ex: ["purchase", "withdrawal"]
   # - currency_code [string, default 'BRL']: code of the currency that the rule amount refers to. ex: 'BRL' or 'USD'
   # - categories [list of MerchantCategories, default nil]: merchant categories accepted by the rule. ex: [MerchantCategory(code='fastFoodRestaurants')]
   # - countries [list of MerchantCountries, default nil]: countries accepted by the rule. ex: [MerchantCountry(code='BRA')]
@@ -26,7 +26,7 @@ module StarkBank
   # - currency_symbol [string]: currency symbol. ex: 'R$'
   # - currency_name [string]: currency name. ex: 'Brazilian Real'
   class CorporateRule < StarkCore::Utils::Resource
-    attr_reader :name, :interval, :amount, :currency_code, :counter_amount, :currency_name, :currency_symbol,
+    attr_reader :name, :interval, :amount, :currency_code, :counter_amount, :schedule, :purposes, :currency_name, :currency_symbol,
                 :categories, :countries, :methods
     def initialize(
       name:, amount:, id: nil, interval: nil, schedule: nil, purposes: nil, currency_code: nil, categories: nil, countries: nil, methods: nil,
