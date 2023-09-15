bea3032a5235f950c965d565e3b6420a722c6429
luistarkbank <luis.almeida@starkbank.com>
2023-09-15 13:41:46 -0300

    Remove branchCode and AccountNumber paymentPreview parameters

--- lib/payment_preview/brcode_preview.rb
+++ lib/payment_preview/brcode_preview.rb
@@ -14,8 +14,6 @@ module StarkBank
     # - name [string]: Payment receiver name. ex: 'Tony Stark'
     # - tax_id [string]: Payment receiver tax ID. ex: '012.345.678-90'
     # - bank_code [string]: Payment receiver bank code. ex: '20018183'
-    # - branch_code [string]: Payment receiver branch code. ex: '0001'
-    # - account_number [string]: Payment receiver account number. ex: '1234567'
     # - account_type [string]: Payment receiver account type. ex: 'checking'
     # - allow_change [bool]: If True, the payment is able to receive amounts that are different from the nominal one. ex: True or False
     # - amount [integer]: Value in cents that this payment is expecting to receive. If 0, any value is accepted. ex: 123 (= R$1,23)
@@ -26,14 +24,12 @@ module StarkBank
     # - discount_amount [integer]: Current discount value in cents that this payment is expecting. ex: 123 (= R$1,23)
     # - reconciliation_id [string]: Reconciliation ID linked to this payment. ex: 'txId', 'payment-123'
     class BrcodePreview < StarkCore::Utils::SubResource
-      attr_reader :status, :name, :tax_id, :bank_code, :branch_code, :account_number, :account_type, :allow_change, :amount, :nominal_amount, :interest_amount, :fine_amount, :reduction_amount, :discount_amount, :reconciliation_id
+      attr_reader :status, :name, :tax_id, :bank_code, :account_type, :allow_change, :amount, :nominal_amount, :interest_amount, :fine_amount, :reduction_amount, :discount_amount, :reconciliation_id
       def initialize(status:, name:, tax_id:, bank_code:, branch_code:, account_number:, account_type:, allow_change:, amount:, nominal_amount:, interest_amount:, fine_amount:, reduction_amount:, discount_amount:, reconciliation_id:)
         @status = status
         @name = name
         @tax_id = tax_id
         @bank_code = bank_code
-        @branch_code = branch_code
-        @account_number = account_number
         @account_type = account_type
         @allow_change = allow_change
         @amount = amount
@@ -54,8 +50,6 @@ module StarkBank
               name: json['name'],
               tax_id: json['tax_id'],
               bank_code: json['bank_code'],
-              branch_code: json['branch_code'],
-              account_number: json['account_number'],
               account_type: json['account_type'],
               allow_change: json['allow_change'],
               amount: json['amount'],
